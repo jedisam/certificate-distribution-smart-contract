9019413d1e4cfd7ff308af1c360a6d7a75bc1cdb
Yididiya <yidisam18@gmail.com>
2022-06-14 08:46:06 +0300

    git ignore files

--- setup.py
+++ setup.py
@@ -3,28 +3,27 @@
 
 from setuptools import setup, find_packages
 
-with open('README.md') as readme_file:
+with open("README.md") as readme_file:
     readme = readme_file.read()
 
-requirements = ['pytest==7.1.1']
+requirements = ["pytest==7.1.1"]
 
-test_requirements = ['pandas', 'matplotlib', 'alogorand-sdk',
-                     , 'sql', 'pytest>=3', ]
+test_requirements = ["pandas", "matplotlib", "alogorand-sdk", "sql", "pytest>=3"]
 
 setup(
     author="Yididiya",
     email="yidisam18@gmail.com",
-    python_requires='>=3.6',
+    python_requires=">=3.6",
     description="Web3 project",
     install_requires=requirements,
     long_description=readme,
     include_package_data=True,
-    keywords='blockchain, algorand, decentralized, pytest',
-    name='W3',
-    packages=find_packages(include=['src', 'src.*']),
-    test_suite='Tests',
+    keywords="blockchain, algorand, decentralized, pytest",
+    name="W3",
+    packages=find_packages(include=["src", "src.*"]),
+    test_suite="Tests",
     tests_require=test_requirements,
-    url='https://github.com/abtesting10academy/W3',
-    version='0.1.0',
+    url="https://github.com/jedisam/W3",
+    version="0.1.0",
     zip_safe=False,
 )
