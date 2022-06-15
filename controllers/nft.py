eee628488af59cf734ce36ad6d02c3421ae49a48
Yididiya <yidisam18@gmail.com>
2022-06-15 21:52:17 +0300

    nft controller edit

--- controllers/nft.py
+++ controllers/nft.py
@@ -184,7 +184,7 @@ class Nft:
                 print(json.dumps(scrutinized_asset, indent=4))
                 break
 
-    def opt_in(self, asset_id):
+    def opt_in_non_fungible_token(self, asset_id):
         # Opt-in to the network
         # This is required for the account to be able to transact on the network.
         # OPT-IN
@@ -234,3 +234,33 @@ class Nft:
             # This should now show a holding with a balance of 0.
             self.print_asset_holding(algod_client, self.accounts[3]["pk"], asset_id)
 
+
+    def transfer_non_fungible_token(self, address, asset_id):
+        """Transfer a non-fungible token to a new address."""
+                
+        # TRANSFER ASSET
+        # transfer asset of 10 from account 1 to account 3
+        params = algod_client.suggested_params()
+        # comment these two lines if you want to use suggested params
+        # params.fee = 1000
+        # params.flat_fee = True
+        txn = AssetTransferTxn(
+            sender=accounts[1]['pk'],
+            sp=params,
+            receiver=address,
+            amt=10,
+            index=asset_id)
+        stxn = txn.sign(accounts[1]['sk'])
+        # Send the transaction to the network and retrieve the txid.
+        try:
+            txid = algod_client.send_transaction(stxn)
+            print("Signed transaction with txID: {}".format(txid))
+            # Wait for the transaction to be confirmed
+            confirmed_txn = wait_for_confirmation(algod_client, txid, 4) 
+            print("TXID: ", txid)
+            print("Result confirmed in round: {}".format(confirmed_txn['confirmed-round']))
+        except Exception as err:
+            print(err)
+        # The balance should now be 10.
+        print_asset_holding(algod_client, accounts[3]['pk'], asset_id)
+        
